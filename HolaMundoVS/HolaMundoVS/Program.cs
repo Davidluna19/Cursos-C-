a9d8be2212efd5a3fff30698981e170acd803d2a
David Luna <davidluna.19@gmail.com>
2018-11-30 17:22:20 -0300

    First Commit
    Creación del  Repó. Hola mundo.

---
+++ HolaMundoVS/HolaMundoVS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundoVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoy es: " + DateTime.Today.Day + " del " + DateTime.Today.Month);
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadKey();
+        }
+    }
+}
